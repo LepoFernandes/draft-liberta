3261299155f17c5f73aaa68f36ac0e5747032431
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-06-11 17:18:03 -0300

    Terminando a DraftScreen

--- draft-liberta/src/components/DraftScreen.jsx
+++ draft-liberta/src/components/DraftScreen.jsx
@@ -3,22 +3,57 @@ import "../styles/DraftScreen.css"
 export default function DraftScreen() {
     return (
         <div className="draft-screen">
-            <h1>Draft Screen</h1>
+            <h1>Draft Liberta</h1>
 
             <div className="draft-content">
                 <div className="field">
-                    <h2>CAMPO</h2>
+                    <div className="line">
+                        <div className="pos">GK</div>
+                    </div>
+
+                    <div className="line">
+                        <div className="pos">LB</div>
+                        <div className="pos">CB</div>
+                        <div className="pos">CB</div>
+                        <div className="pos">RB</div>
+                    </div>
+
+                    <div className="line">
+                        <div className="pos">CDM</div>
+                    </div>
+
+                    <div className="line">
+                        <div className="pos">CM</div>
+                        <div className="pos">CM</div>
+                    </div>
+
+                    <div className="line">
+                        <div className="pos">LW</div>
+                        <div className="pos">ST</div>
+                        <div className="pos">RW</div>
+                    </div>
                 </div>
 
-                <div className="squad">
-                    <h2>ELENCO</h2>
+                <div className="player-list">
+                    <h2>TIME</h2>
+                    <ul className="list">
+                        <li className="item">Jogador 1</li>
+                        <li className="item">Jogador 2</li>
+                        <li className="item">Jogador 3</li>
+                        <li className="item">Jogador 4</li>
+                        <li className="item">Jogador 5</li>
+                        <li className="item">Jogador 6</li>
+                        <li className="item">Jogador 7</li>
+                        <li className="item">Jogador 8</li>
+                        <li className="item">Jogador 9</li>
+                        <li className="item">Jogador 10</li>
+                        <li className="item">Jogador 11</li>
+                    </ul>
+                    <button className="button-draft">
+                        SORTEAR TIMES
+                    </button>
                 </div>
             </div>
-
-            <button>
-                SORTEAR TIMES
-            </button>
-
         </div>
     )
 }
