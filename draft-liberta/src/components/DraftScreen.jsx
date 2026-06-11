60b0471208d36db5729251fefbc260bd5e96d40f
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-06-11 14:47:17 -0300

    Comecando a implementar a draftScreen

--- draft-liberta/src/components/DraftScreen.jsx
+++ draft-liberta/src/components/DraftScreen.jsx
@@ -1,10 +1,24 @@
-
+import "../styles/DraftScreen.css"
 
 export default function DraftScreen() {
     return (
-        <div>
+        <div className="draft-screen">
             <h1>Draft Screen</h1>
-            <p>Em construção...</p>
+
+            <div className="draft-content">
+                <div className="field">
+                    <h2>CAMPO</h2>
+                </div>
+
+                <div className="squad">
+                    <h2>ELENCO</h2>
+                </div>
+            </div>
+
+            <button>
+                SORTEAR TIMES
+            </button>
+
         </div>
     )
 }
