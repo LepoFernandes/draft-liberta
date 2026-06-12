9ee81aec9ba8dea4b6c43fe804b7e75bcf1e5242
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-06-12 12:15:52 -0300

    Implementando jogadores selecionados e pos corretas

--- draft-liberta/src/components/DraftScreen.jsx
+++ draft-liberta/src/components/DraftScreen.jsx
@@ -1,59 +1,212 @@
-import "../styles/DraftScreen.css"
+import { useState } from "react";
+import { teams } from "../data/Teams";
+
+import "../styles/DraftScreen.css";
 
 export default function DraftScreen() {
+
+    const [currentTeam, setCurrentTeam] = useState(null)
+
+    const [selectedPlayers, setSelectedPlayers] = useState([])
+
+    function sortearTme() {
+        setCurrentTeam(teams[0]);
+    }
+
+    function selecionarJogador(player) {
+        const posicaoOcupada = selectedPlayers.some(
+            (selected) => selected.position === player.position
+        );
+
+        if (posicaoOcupada) {
+            return;
+        }
+
+        setSelectedPlayers((prev) => [...prev, player]);
+    }
+
+    function getSelectedPlayer(position) {
+        return selectedPlayers.find(
+            (player) => player.position === position
+        );
+    }
+
+    function jogadorSelecionado(player) {
+        return selectedPlayers.some(
+            (selected) => selected.name === player.name)
+    }
+
     return (
         <div className="draft-screen">
             <h1>Draft Liberta</h1>
 
+            {selectedPlayers.length < 11 ? (
+                <p>
+                    Jogadores escolhidos: {selectedPlayers.length}/11
+                </p>
+            ) : (
+                <p className="complete-team">
+                    🏆 ELENCO COMPLETO
+                </p>
+            )}
+
             <div className="draft-content">
+
                 <div className="field">
+
                     <div className="line">
-                        <div className="pos">GK</div>
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("GK")?.overall || "GK"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("GK")?.name || ""}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="line">
-                        <div className="pos">LB</div>
-                        <div className="pos">CB</div>
-                        <div className="pos">CB</div>
-                        <div className="pos">RB</div>
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("LB")?.overall || "LB"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("LB")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("CB1")?.overall || "CB"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("CB1")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("CB2")?.overall || "CB"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("CB2")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("RB")?.overall || "RB"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("RB")?.name || ""}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="line">
-                        <div className="pos">CDM</div>
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("CDM")?.overall || "CDM"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("CDM")?.name || ""}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="line">
-                        <div className="pos">CM</div>
-                        <div className="pos">CM</div>
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("CM1")?.overall || "CM"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("CM1")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("CM2")?.overall || "CM"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("CM2")?.name || ""}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="line">
-                        <div className="pos">LW</div>
-                        <div className="pos">ST</div>
-                        <div className="pos">RW</div>
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("LW")?.overall || "LW"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("LW")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("ST")?.overall || "ST"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("ST")?.name || ""}
+                            </div>
+                        </div>
+
+                        <div className="pos">
+                            <div className="pos-overall">
+                                {getSelectedPlayer("RW")?.overall || "RW"}
+                            </div>
+                            <div className="pos-name">
+                                {getSelectedPlayer("RW")?.name || ""}
+                            </div>
+                        </div>
                     </div>
+
                 </div>
 
                 <div className="player-list">
-                    <h2>TIME</h2>
-                    <ul className="list">
-                        <li className="item">Jogador 1</li>
-                        <li className="item">Jogador 2</li>
-                        <li className="item">Jogador 3</li>
-                        <li className="item">Jogador 4</li>
-                        <li className="item">Jogador 5</li>
-                        <li className="item">Jogador 6</li>
-                        <li className="item">Jogador 7</li>
-                        <li className="item">Jogador 8</li>
-                        <li className="item">Jogador 9</li>
-                        <li className="item">Jogador 10</li>
-                        <li className="item">Jogador 11</li>
-                    </ul>
-                    <button className="button-draft">
+
+                    <h2>
+                        {currentTeam ? currentTeam.name : "Vamos começar"}
+                    </h2>
+
+                    {!currentTeam && (
+                        <p className="empty-message">
+                            Clique em "Sortear Time" para começar seu draft.
+                        </p>
+                    )}
+
+                    {currentTeam && (
+                        <ul className="list">
+                            {currentTeam.players.map((player) => (
+                                <li
+                                    key={player.name}
+                                    className={`item ${jogadorSelecionado(player) ? "selected" : ""
+                                        }`}
+                                    onClick={() => {
+                                        if (!jogadorSelecionado(player)) {
+                                            selecionarJogador(player);
+                                        }
+                                    }}
+                                >
+                                    {player.name} - {player.overall}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+
+                    <button
+                        className="button-draft"
+                        onClick={sortearTme}
+                    >
                         SORTEAR TIMES
                     </button>
+
                 </div>
+
             </div>
+
         </div>
     )
 }
---
+++ draft-liberta/src/data/Teams.js
@@ -0,0 +1,19 @@
+export const teams = [
+    {
+        id : 1,
+        name: "Grêmio 2017",
+        players: [
+      { name: "Grohe", position: "GK", overall: 84 },
+      { name: "Cortez", position: "LB", overall: 80 },
+      { name: "Geromel", position: "CB1", overall: 88 },
+      { name: "Kannemann", position: "CB2", overall: 86 },
+      { name: "Edilson", position: "RB", overall: 82 },
+      { name: "Arthur", position: "CM1", overall: 87 },
+      { name: "Maicon", position: "CDM", overall: 84 },
+      { name: "Ramiro", position: "CM2", overall: 80 },
+      { name: "Everton", position: "LW", overall: 88 },
+      { name: "Luan", position: "ST", overall: 91 },
+      { name: "Fernandinho", position: "RW", overall: 80 },
+    ]
+    }
+];
